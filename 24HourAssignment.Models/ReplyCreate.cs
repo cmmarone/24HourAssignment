3977b89ed88d3dfe2ef3e278df3d70acc66467d9
vscott1989 <vscott1811@gmail.com>
2021-04-13 15:21:31 -0400

    VanessaUpdatedModels

---
+++ 24HourAssignment.Models/ReplyCreate.cs
@@ -0,0 +1,22 @@
+﻿using _24HourAssignment.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourAssignment.Models
+{
+    public class ReplyCreate
+    {
+        [Required]
+        public string ReplyText { get; set; }
+
+
+        public int? CommentId { get; set; }
+
+        public Comment Comment { get; set; }
+    }
+}
---
+++ 24HourAssignment.Models/ReplyEdit.cs
@@ -0,0 +1,27 @@
+﻿using _24HourAssignment.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourAssignment.Models
+{
+    public class ReplyEdit
+    {
+     
+        public int ReplyId { get; set; }
+
+        
+        public int CommentId { get; set; }
+
+        public  Comment Comment { get; set; }
+
+        
+        public string ReplyText { get; set; }
+
+      
+        public Guid AuthorId { get; set; }
+
+    }
+}
