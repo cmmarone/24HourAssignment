234e186cb7b074e0912d16815f7ef071f309d46f
vscott1989 <vscott1811@gmail.com>
2021-04-13 11:46:29 -0400

    updateVanessaBranch

---
+++ 24HourAssignment.Data/Reply.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourAssignment.Data
+{
+    public class Reply
+    {
+        [Key]
+        public int ReplyId { get; set; }
+
+        [ForeignKey(nameof(Comment))]
+        public int CommentId { get; set; }
+
+        public virtual Comment Comment { get; set; }
+
+        [Required]
+        public string CommentText { get; set; }
+
+        [Required]
+        public  Guid AuthorId { get; set; }
+
+
+    }
+}
