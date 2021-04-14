f9c9fd8abcfa0ff0d0109d99a55a1d569bd4a66c
vscott1989 <vscott1811@gmail.com>
2021-04-14 09:54:59 -0400

    FinalUpdate

--- 24HourAssignment.WebAPI/Controllers/ReplyController.cs
+++ 24HourAssignment.WebAPI/Controllers/ReplyController.cs
@@ -1,17 +1,27 @@
-﻿using System;
+﻿using _24HourAssignment.Models;
+using _24HourAssignment.Services;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
+using AuthorizeAttribute = System.Web.Http.AuthorizeAttribute;
 
 namespace _24HourAssignment.WebAPI.Controllers
 {
+    [Authorize]
     public class ReplyController : Controller
     {
-        // GET: Reply
-        public ActionResult Index()
+
+        private ReplyService CreateReplyService()
         {
-            return View();
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new ReplyService(userId);
+            return service;
         }
+
+
     }
 }
