4d8a77e139a42600a726d61cb3b4e2a93fa37616
daniellejackson <weathers.danielle@ymail.com>
2021-04-14 09:56:46 -0400

    FinalProjectDani

---
+++ 24HourAssignment.WebAPI/Controllers/PostController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _24HourAssignment.WebAPI.Controllers
+{
+    
+    [Authorize]
+    public class PostController : ApiController
+    {
+
+
+        public IHttpActionResult Get()
+        {
+            NoteServices noteService = CreateNoteService();
+            var notes = noteService.GetNotes();
+            return Ok(notes);
+        }
+        public IHttpActionResult Get(int id)
+        {
+            NoteServices noteService = CreateNoteService();
+            var note = noteService.GetNoteById(id);
+            return Ok(note);
+        }
+
+        public IHttpActionResult Post(NoteCreate note)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateNoteService();
+
+            if (!service.CreateNote(note))
+                return InternalServerError();
+
+            return Ok();
+        }
+        private NoteServices CreateNoteService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var noteService = new NoteServices(userId);
+            return noteService;
+        }
+
+        public IHttpActionResult Put(NoteEdit note)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateNoteService();
+
+            if (!service.UpdateNote(note))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateNoteService();
+
+            if (!service.DeleteNote(id))
+                return InternalServerError();
+
+            return Ok();
+        }
+    }
+
+
+}
